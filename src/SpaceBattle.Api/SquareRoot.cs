853536cfd59334a83372b46993cadb68627c6e72
Владислав Дериглазов <kredis31@gmail.com>
2024-01-18 21:32:50 +0300

    SquareRootTest

---
+++ src/SpaceBattle.Api/SquareRoot.cs
@@ -0,0 +1,33 @@
+﻿namespace SpaceBattle.Api;
+public static class SquareRoot
+{
+
+    private const double e = 1e-5;
+
+    public static double[] Solve(double a, double b, double c)
+    {
+        if (Math.Abs(a) < e)
+        {
+            throw new ArgumentException($"{a} не равно 0", nameof(a));
+        }
+        var discriminant = Math.Pow(b, 2) - 4 * a * c;
+        if (discriminant < 0)
+        {
+            return [];
+        }
+
+        if (discriminant == 0)
+        {
+            var x1 = -b / (2 * a);
+            return [x1];
+        }
+        else
+        {
+            var x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            var x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+            return [x1, x2];
+        }
+
+
+    }
+}
---
+++ src/SpaceBattle.Test/SquareRootTest.cs
@@ -0,0 +1,47 @@
+using SpaceBattle.Api;
+using Xunit;
+
+namespace SpaceBattle.Test;
+
+public class SquareRootTest
+{
+    [InlineData(1, 0, 1)]
+    [Theory]
+    public void Solve_Empty(double a, double b, double c)
+    {
+        var act = SquareRoot.Solve(a, b, c);
+
+        Assert.Empty(act);
+
+    }
+
+    [InlineData(1, 0, -1, 1, -1)]
+    [Theory]
+    public void Solve_TwoRoot(double a, double b, double c, double x1, double x2)
+    {
+        var act = SquareRoot.Solve(a, b, c);
+
+        Assert.Equal(x1, act[0]);
+        Assert.Equal(x2, act[1]);
+
+
+    }
+    [InlineData(1, 2, 1, -1)]
+    [Theory]
+    public void Solve_OneRoot(double a, double b, double c, double x1)
+    {
+        var act = SquareRoot.Solve(a, b, c);
+
+        Assert.Equal(x1, act[0]);
+
+    }
+    [InlineData(1e-6, 5, 5)]
+    [InlineData(0.0000001, 5, 5)]
+    [InlineData(uint.MinValue, double.MaxValue, double.MaxValue)]
+    [Theory]
+    public void Solve_Exception(double a, double b, double c)
+    {
+        Assert.Throws<ArgumentException>(() => SquareRoot.Solve(a, b, c));
+
+    }
+}
