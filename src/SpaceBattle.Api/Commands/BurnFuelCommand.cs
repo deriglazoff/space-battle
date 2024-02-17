4e4fd9dfee4f855e961cfd0b64999df1affad814
Владислав Дериглазов <kredis31@gmail.com>
2024-02-17 16:41:33 +0300

    MacroCommand

---
+++ src/SpaceBattle.Api/Commands/BurnFuelCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace SpaceBattle.Api.Commands;
+
+public class BurnFuelCommand(IFuel ship) : ICommand
+{
+    public void Execute()
+    {
+        ship.Fuel -= ship.NeedFuel;
+    }   
+}
---
+++ src/SpaceBattle.Api/Commands/CheckFuelCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaceBattle.Api.Commands;
+
+public class CheckFuelCommand(IFuel ship) : ICommand
+{
+    public void Execute()
+    {
+        if (ship.Fuel < ship.NeedFuel)
+            throw new ValidationException();
+    }
+}
