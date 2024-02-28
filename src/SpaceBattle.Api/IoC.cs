967d8350e634ffb40de3006b455ba6de166c4636
Владислав Дериглазов <kredis31@gmail.com>
2024-02-28 23:08:41 +0300

    ioc

---
+++ src/SpaceBattle.Api/IoC.cs
@@ -0,0 +1,48 @@
+﻿using SpaceBattle.Api.Commands;
+using System.Collections.Concurrent;
+
+namespace SpaceBattle.Api;
+
+public class IoC : IResolvable
+{
+    public ConcurrentDictionary<string, Scope> Scopes { get; }
+    public ThreadLocal<Scope> CurrentScope { get; }
+
+    public IoC()
+    {
+        Scopes = new();
+        CurrentScope = new() { Value = new Scope("DefaultScope") };
+        new IocRegisterCommand(CurrentScope.Value?.Dependencies,
+                                        "IoC.Register",
+                                        args => new IocRegisterCommand(CurrentScope.Value?.Dependencies,
+                                          args[0] as string,
+                                          args[1] as Func<object[], object>)
+                             ).Execute();
+
+        new IocRegisterCommand(CurrentScope.Value?.Dependencies,
+                                "Scopes.New",
+                                args => new ScopeRegisterCommand(Scopes, args[0].ToString())
+                     ).Execute();
+
+        new IocRegisterCommand(CurrentScope.Value?.Dependencies,
+                        "Scopes.Current",
+                        args => new ScopeCurrentSetCommand(this, Scopes, args[0].ToString())
+             ).Execute();
+    }
+    public T? Resolve<T>(string key, params object[] args)
+    {
+        if (CurrentScope.Value == null || key == null || !CurrentScope.Value.Dependencies.TryGetValue(key, out object? value))
+        {
+            throw new Exception($"No operation with key {key}");
+        }
+
+        var function = (Func<object[], object>)value;
+        return (T)function.Invoke(args);
+    }
+
+    public class Scope(string name)
+    {
+        public string Name { get; } = name;
+        public IDictionary<string, object> Dependencies { get; } = new Dictionary<string, object>();
+    }
+}
---
+++ src/SpaceBattle.Test/IocTest.cs
@@ -0,0 +1,116 @@
+﻿using SpaceBattle.Api.Commands;
+using SpaceBattle.Api;
+using Xunit;
+using System.Numerics;
+
+namespace SpaceBattle.Test;
+
+public class IocTest {
+    [Fact]
+    public void ErrorThrowsWhenOperationKeyNotFound()
+    {
+        var ioc = new IoC();
+
+        var act = () => ioc.Resolve<object>("test");
+
+        Assert.Throws<Exception>(act);
+    }
+
+    [Fact]
+    public void MoveCommandRegistersAndResolves()
+    {
+        var ioc = new IoC();
+        var ship = new SpaceShip
+        {
+            Fuel = 10,
+            NeedFuel = 10,
+            Position = new Vector2(12, 5),
+            Velocity = new Vector2(-7, 3)
+        };
+        ioc.Resolve<ICommand>("IoC.Register",
+                              "Move",
+                              (Func<object[], object>)(args => new MacroCommand(new CheckFuelCommand((IFuel)args[0]), new BurnFuelCommand((IFuel)args[0]), new MoveCommand((IMovable)args[0]))))
+           .Execute();
+
+        var moveCommand = ioc.Resolve<ICommand>("Move", ship);
+
+        Assert.IsType<MacroCommand>(moveCommand);
+    }
+
+    [Fact]
+    public void ScopeRegisters()
+    {
+        var ioc = new IoC();
+
+        Assert.Empty(ioc.Scopes);
+        ioc.Resolve<ICommand>("Scopes.New", "id_1")
+           .Execute();
+
+        Assert.NotEmpty(ioc.Scopes);
+    }
+
+    [Fact]
+    public void ErrorThrowsWhenTryToRegisterDuplicatedScope()
+    {
+        var ioc = new IoC();
+
+        ioc.Resolve<ICommand>("Scopes.New", "id_1")
+           .Execute();
+        Action act = () => ioc.Resolve<ICommand>("Scopes.New", "id_1")
+                              .Execute();
+
+        Assert.Throws<Exception>(act);
+    }
+
+    [Fact]
+    public void RegisteredScopeSetsAsCurrent()
+    {
+        var ioc = new IoC();
+
+        ioc.Resolve<ICommand>("Scopes.New", "id_1")
+           .Execute();
+        ioc.Resolve<ICommand>("Scopes.Current", "id_1")
+           .Execute();
+
+        Assert.Equal("id_1", ioc.CurrentScope.Value?.Name);
+    }
+
+    [Fact]
+    public void ErrorThrowsWhenTryingSetNotRegisteredScope()
+    {
+        var ioc = new IoC();
+
+        Action act = () => ioc.Resolve<ICommand>("Scopes.Current", "id_1")
+                              .Execute();
+
+        Assert.Throws<Exception>(act);
+    }
+
+    [Fact]
+    public void CurrentScopeSetsForEachThread()
+    {
+        var ioc = new IoC();
+
+        ioc.Resolve<ICommand>("Scopes.New", "id_1")
+           .Execute();
+        ioc.Resolve<ICommand>("Scopes.New", "id_2")
+           .Execute();
+
+        Task.Factory.StartNew(() =>
+        {
+            ioc.Resolve<ICommand>("Scopes.Current", "id_1")
+               .Execute();
+
+            Assert.Equal("id_1", ioc.CurrentScope.Value?.Name);
+        });
+        Task.Factory.StartNew(() =>
+        {
+            ioc.Resolve<ICommand>("Scopes.Current", "id_2")
+               .Execute();
+
+            Assert.Equal("id_2", ioc.CurrentScope.Value?.Name);
+        });
+
+        Assert.Equal("DefaultScope", ioc.CurrentScope.Value?.Name);
+    }
+}
